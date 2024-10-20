f9fe45eb908ad6ec4c18673cdf9d00106ae53b09
zhanghefan123 <1976939885@qq.com>
2024-10-20 17:17:15 +0800

    update

---
+++ images/normal_node/normal_node/main/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/signal"
+	"zhanghefan123/security/normal_satellite/modules/frr"
+	"zhanghefan123/security/normal_satellite/modules/srv6"
+)
+
+func main() {
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, os.Interrupt)
+	defer signal.Stop(signalChan)
+
+	// 启动流程
+	// =======================================================
+	PrintExitLogo()
+	frr.Start()
+	srv6.Start()
+	// =======================================================
+
+	<-signalChan
+
+	// 删除流程
+	// =======================================================
+	PrintRemovedLogo()
+	// =======================================================
+}
+
+func PrintExitLogo() {
+	fmt.Println("<------------------------------------->")
+	fmt.Println("            enter ctl+c exit           ")
+	fmt.Println("<------------------------------------->")
+}
+
+func PrintRemovedLogo() {
+	fmt.Println("<------------------------------------->")
+	fmt.Println("            satellite killed           ")
+	fmt.Println("<------------------------------------->")
+}
